af4a0bcdbb0c627efcc4fdb4af629e487325ba0e
Shadowk1ngX <94004343+Shadowk1ngX@users.noreply.github.com>
2024-11-26 03:27:56 -0500

    Firebase setup

--- FirebaseScript.py
+++ FirebaseScript.py
@@ -28,6 +28,30 @@ def GrabAllItems(CollectionName):
         print(f"Document Data: {doc_data['data']}")
         print()
 
+def GrabAllItems2(CollectionName):
+    print("Grabbing All Items")
+    try:
+        # Use get() to fetch the documents
+        docs = db.collection(CollectionName).get()
+        print("Documents Retrieved")
+        DocList = []
 
-GrabAllItems("products")
+        print("Insert Data")
+        for doc in docs:
+            doc_data = doc.to_dict()
+            doc_data["id"] = doc.id
+            DocList.append(doc_data)
+
+        print("Show Data")
+        for doc_data in DocList:
+            print(f"Document ID: {doc_data['id']}")
+            print(f"Document Data: {doc_data}")
+            print()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+
+GrabAllItems2("products")
 print("RAN")
