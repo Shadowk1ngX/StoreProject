3e9b58e278b3f6bc0aab82b51f25e426c180073d
Shadowk1ngX <94004343+Shadowk1ngX@users.noreply.github.com>
2024-11-29 22:27:28 -0500

    idjxfg

--- authentication.py
+++ authentication.py
@@ -51,7 +51,7 @@ def signup(email, password):
         error_message = get_error_message(error_str)
         if "EMAIL_EXISTS" in error_message:
             message = "Email already exists. Please login instead."
-            return False, message
+            return False, message, None
         else:
             print(e)
             print("Error during signup:", error_message)
